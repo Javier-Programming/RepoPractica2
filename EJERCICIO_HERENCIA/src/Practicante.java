0fa1756572f63b0d7f3d42a46a11c83626fd4647
Jormenok2 <Eduardovargasmail@gmail.com>
2025-10-27 21:13:33 -0500

    feat: Se crea la funcion toString

--- EJERCICIO_HERENCIA/src/Practicante.java
+++ EJERCICIO_HERENCIA/src/Practicante.java
@@ -1,8 +1,27 @@
 public class Practicante {
 
     private int id;
-    private String nombreString
-    public Practicante(int nombre, String nombre){
-        
+    private String nombre;
+    public Practicante(int id, String nombre){
+        this.id = id;
+        this.nombre = nombre;
     }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString(){
+    return "Practicante: " +this.id+
+            " - Nombre: " +this.nombre;
+        }
 }
