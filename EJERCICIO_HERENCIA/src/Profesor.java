29f3203e432b400a8f214bafab6dcb66b6e87c2c
andrestowers03-gif <andrestowers03@gmail.com>
2025-10-27 21:15:25 -0500

    se agrego un toString y una nueva funcionalidad llamada saludo

--- EJERCICIO_HERENCIA/src/Profesor.java
+++ EJERCICIO_HERENCIA/src/Profesor.java
@@ -1,29 +1,37 @@
 public class Profesor {
-//atributos 
-private int id;
-private String nombre;
+    // atributos
+    private int id;
+    private String nombre;
 
-//constructor 
-public Profesor(int id, String nombre) {
-    this.id = id;
-    this.nombre = nombre;
-}
-//getter y setter
+    // constructor
+    public Profesor(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+    // getter y setter
 
-public int getId() {
-    return id;
-}
+    public int getId() {
+        return id;
+    }
 
-public void setId(int id) {
-    this.id = id;
-}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-public String getNombre() {
-    return nombre;
-}
+    public String getNombre() {
+        return nombre;
+    }
 
-public void setNombre(String nombre) {
-    this.nombre = nombre;
-}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String saludo(){
+        return "hola" + nombre;
+    }
 
+    // agregar toString
+    @Override
+    public String toString() {
+        return "Andres Torres Diaz el mejor :)";
+    }
 }
